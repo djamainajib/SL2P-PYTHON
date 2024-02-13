38e0fbc36e3de976809aae30d5d7fc3a80379b2a
djamainajib <33295871+djamainajib@users.noreply.github.com>
2024-02-13 17:19:18 -0500

    Add files via upload

--- tools/dictionariesSL2P.py
+++ tools/dictionariesSL2P.py
@@ -13,13 +13,10 @@ def make_collection_options(fc):
         'S2_SR': {
         "name": 'S2_SR',
         "description": 'Sentinel 2A',
-        "Cloudcover": 'CLOUDY_PIXEL_PERCENTAGE',
-        "Watercover": 'WATER_PERCENTAGE',
         "sza": 'MEAN_SOLAR_ZENITH_ANGLE',
         "vza": 'MEAN_INCIDENCE_ZENITH_ANGLE_B8A',
         "saa": 'MEAN_SOLAR_AZIMUTH_ANGLE', 
         "vaa": 'MEAN_INCIDENCE_AZIMUTH_ANGLE_B8A',
-        "VIS_OPTIONS": 'VIS_OPTIONS',
         "Collection_SL2P": fc.s2_createFeatureCollection_estimates(),   
         "Collection_SL2Perrors": fc.s2_createFeatureCollection_errors(),        
         "sl2pDomain": fc.s2_createFeatureCollection_domains(),   
@@ -31,13 +28,10 @@ def make_collection_options(fc):
         'S2_SR_10m': {
         "name": 'S2_SR_10m',
         "description": 'Sentinel 2A',
-        "Cloudcover": 'CLOUDY_PIXEL_PERCENTAGE',
-        "Watercover": 'WATER_PERCENTAGE',
         "sza": 'MEAN_SOLAR_ZENITH_ANGLE',
         "vza": 'MEAN_INCIDENCE_ZENITH_ANGLE_B8A',
         "saa": 'MEAN_SOLAR_AZIMUTH_ANGLE', 
         "vaa": 'MEAN_INCIDENCE_AZIMUTH_ANGLE_B8A',
-        "VIS_OPTIONS": 'VIS_OPTIONS',
         "Collection_SL2P": fc.s2_10m_createFeatureCollection_estimates(),
         "Collection_SL2Perrors": fc.s2_10m_createFeatureCollection_errors(),  
         "sl2pDomain": fc.s2_10m_createFeatureCollection_domains(),
@@ -54,8 +48,6 @@ def make_net_options():
         'Albedo': {
             "S2_SR": {
                 "Name": 'Albedo',
-                "errorName": 'errorAlbedo',
-                "maskName": 'maskAlbedo',
                 "description": 'Black sky albedo',
                 "variable": 6,
                 "inputBands":      ['cosVZA', 'cosSZA', 'cosRAA', 'B03', 'B04', 'B05', 'B06', 'B07', 'B8A', 'B11', 'B12'],
@@ -64,8 +56,6 @@ def make_net_options():
             },
             "S2_SR_10m": {
                 "Name": 'Albedo',
-                "errorName": 'errorAlbedo',
-                "maskName": 'maskAlbedo',
                 "description": 'Black sky albedo',
                 "variable": 6,
                 "inputBands":      ['cosVZA', 'cosSZA', 'cosRAA', 'B02', 'B03', 'B04', 'B08'],
@@ -86,8 +76,6 @@ def make_net_options():
             },
             "S2_SR_10m": {
                 "Name": 'fAPAR',
-                "errorName": 'errorfAPAR',
-                "maskName": 'maskfAPAR',
                 "description": 'Fraction of absorbed photosynthetically active radiation',
                 "variable": 2,
                 "inputBands":      ['cosVZA', 'cosSZA', 'cosRAA', 'B02', 'B03', 'B04', 'B08'],
@@ -98,8 +86,6 @@ def make_net_options():
         'fCOVER': {
             "S2_SR": {
                 "Name": 'fCOVER',
-                "errorName": 'errorfCOVER',
-                "maskName": 'maskfCOVER',
                 "description": 'Fraction of canopy cover',
                 "variable": 3,
                 "inputBands":      ['cosVZA', 'cosSZA', 'cosRAA', 'B03', 'B04', 'B05', 'B06', 'B07', 'B8A', 'B11', 'B12'],
@@ -108,8 +94,6 @@ def make_net_options():
             },
             "S2_SR_10m": {
                 "Name": 'fCOVER',
-                "errorName": 'errorfCOVER',
-                "maskName": 'maskfCOVER',
                 "description": 'Fraction of canopy cover',
                 "variable": 3,
                 "inputBands":      ['cosVZA', 'cosSZA', 'cosRAA', 'B02', 'B03', 'B04', 'B08'],
@@ -120,8 +104,6 @@ def make_net_options():
         'LAI': {
             "S2_SR": {
                 "Name": 'LAI',
-                "errorName": 'errorLAI',
-                "maskName": 'maskLAI',
                 "description": 'Leaf area index',
                 "variable": 1,
                 "inputBands":      ['cosVZA', 'cosSZA', 'cosRAA', 'B03', 'B04', 'B05', 'B06', 'B07', 'B8A', 'B11', 'B12'],
@@ -130,8 +112,6 @@ def make_net_options():
             },
             "S2_SR_10m": {
                 "Name": 'LAI',
-                "errorName": 'errorLAI',
-                "maskName": 'maskLAI',
                 "description": 'Leaf area index',
                 "variable": 1,
                 "inputBands":      ['cosVZA', 'cosSZA', 'cosRAA', 'B02', 'B03', 'B04', 'B08'],
@@ -142,8 +122,6 @@ def make_net_options():
         'CCC': {
             "S2_SR": {
                 "Name": 'CCC',
-                "errorName": 'errorCCC',
-                "maskName": 'maskCCC',
                 "description": 'Canopy chlorophyll content',
                 "variable": 4,
                 "inputBands":      ['cosVZA', 'cosSZA', 'cosRAA', 'B03', 'B04', 'B05', 'B06', 'B07', 'B8A', 'B11', 'B12'],
@@ -152,8 +130,6 @@ def make_net_options():
             },
             "S2_SR_10m": {
                 "Name": 'CCC',
-                "errorName": 'errorCCC',
-                "maskName": 'maskCCC',
                 "description": 'Canopy chlorophyll content',
                 "variable": 4,
                 "inputBands":      ['cosVZA', 'cosSZA', 'cosRAA', 'B02', 'B03', 'B04', 'B08'],
@@ -164,8 +140,6 @@ def make_net_options():
         'CWC': {
             "S2_SR": {
                 "Name": 'CWC',
-                "errorName": 'errorCWC',
-                "maskName": 'maskCWC',
                 "description": 'Canopy water content',
                 "variable": 5,
                 "inputBands":      ['cosVZA', 'cosSZA', 'cosRAA', 'B03', 'B04', 'B05', 'B06', 'B07', 'B8A', 'B11', 'B12'],
@@ -174,8 +148,6 @@ def make_net_options():
             },
             "S2_SR_10m": {
                 "Name": 'CWC',
-                "errorName": 'errorCWC',
-                "maskName": 'maskCWC',
                 "description": 'Canopy water content',
                 "variable": 5,
                 "inputBands":      ['cosVZA', 'cosSZA', 'cosRAA', 'B02', 'B03', 'B04', 'B08'],
@@ -191,34 +163,32 @@ def make_outputParams():
     # output parameters
     outputParams = {
         'Albedo': {
-            'outputScale': 1000,
             'outputOffset': 0,
             'outputMax': 0.2
         },
         'fAPAR': {
-            'outputScale': 1000,
             'outputOffset': 0,
             'outputMax': 1
         },
         'fCOVER': {
-            'outputScale': 1000,
             'outputOffset': 0,
             'outputMax': 1
         },
         'LAI': {
-            'outputScale': 1000,
             'outputOffset': 0,
             'outputMax': 8
         },
         'CCC': {
-            'outputScale': 1000,
             'outputOffset': 0,
             'outputMax': 600
         },
         'CWC': {
-            'outputScale': 1000,
             'outputOffset': 0,
             'outputMax': 0.55
         },
+        'DASF': {
+            'outputOffset': 0,
+            'outputMax': 1
+        }
     }
     return(outputParams)
